3a1bf9b177f8280e1b6afb5e9badb5f858569421
Tomas Tobar <codetombomb@gmail.com>
2019-12-02 21:19:59 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -31,7 +31,8 @@ end
 
 def matrix_lookup(matrix, row, column)
   # Return the matrix's content at row and column
-  
+  matrix = [ENNET_HOUSE, ENFIELD_TENNIS_ACADEMY]
+  matrix[row][column]
 end
 
 def matrix_update(matrix, row, column, new_value)
